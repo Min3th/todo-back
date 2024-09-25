61a2ca900c33df107d2319b3c9412f8233d2991d
Min3th <minethweerasinghe@gmail.com>
2024-09-25 11:21:39 +0530

    Initial commit for backend

---
+++ toDoApp/Controllers/todoappController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace toDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class todoappController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public todoappController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/todoapp/GetNotes
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from dbo.notes";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoappcon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        // POST: api/todoapp/AddNotes
+        [HttpPost]
+        [Route("AddNotes")]
+        public JsonResult AddNotes([FromForm] string newNotes)
+        {
+            string query = "insert into dbo.notes (description) values(@newNotes)";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoappcon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@newNotes", newNotes);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteNotes")]
+        public JsonResult DeleteNotes(int id)
+        {
+            string query = "delete from dbo.Notes where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoappcon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id",id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
